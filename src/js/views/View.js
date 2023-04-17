d1159755c25f0545d1a073f3de48bc14f9d21475
senichich <sadlovskaanastasia@gmail.com>
2023-04-17 11:27:02 +0300

    Clean

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -1,6 +1,8 @@
-import icons from 'url:../../img/icons.svg';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
 export default class View {
   _data;
+
   /**
    * Render the received object to the DOM
    * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
@@ -13,10 +15,12 @@ export default class View {
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
+
     this._data = data;
     const markup = this._generateMarkup();
 
     if (!render) return markup;
+
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
@@ -53,37 +57,45 @@ export default class View {
   _clear() {
     this._parentElement.innerHTML = '';
   }
+
   renderSpinner() {
-    const markup = `<div class="spinner">
-      <svg>
-        <use href="${icons}#icon-loader"></use>
-      </svg>
-    </div>`;
+    const markup = `
+      <div class="spinner">
+        <svg>
+          <use href="${icons}#icon-loader"></use>
+        </svg>
+      </div>
+    `;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
-  renderError(message = this._erroMessage) {
-    const markup = `<div class="error">
+  renderError(message = this._errorMessage) {
+    const markup = `
+      <div class="error">
         <div>
-        <svg>
-        <use href="${icons}#icon-alert-triangle"></use>
-        </svg>
+          <svg>
+            <use href="${icons}#icon-alert-triangle"></use>
+          </svg>
         </div>
         <p>${message}</p>
-        </div>`;
+      </div>
+    `;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
+
   renderMessage(message = this._message) {
-    const markup = `<div class="message">
+    const markup = `
+      <div class="message">
         <div>
-        <svg>
-        <use href="${icons}#icon-smile"></use>
-        </svg>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
+          </svg>
         </div>
         <p>${message}</p>
-        </div>`;
+      </div>
+    `;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
